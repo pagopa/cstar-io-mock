ea2553ea0357f738c318712ba5f9c66985b31dd6
Giovanni <g.mancini.0@gmail.com>
2023-06-20 11:22:11 +0200

    Create stub microservice (#1)

---
+++ src/test/java/it/pagopa/cstar/iomock/IoMockApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pagopa.cstar.iomock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
