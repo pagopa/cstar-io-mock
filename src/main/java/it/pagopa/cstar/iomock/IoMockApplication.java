f9949e014b933c4d4c488ec3d6672029a9f46af0
Giovanni <g.mancini.0@gmail.com>
2023-06-20 11:21:41 +0200

    Create stub microservice

---
+++ src/main/java/it/pagopa/cstar/iomock/IoMockApplication.java
@@ -0,0 +1,13 @@
+package it.pagopa.cstar.iomock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoMockApplication.class, args);
+	}
+
+}
