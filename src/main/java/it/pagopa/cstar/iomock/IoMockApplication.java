810c740d6e7d3ea72ae11b9698423d582198315c
petretiandrea <petretiandrea@gmail.com>
2023-07-20 17:17:43 +0200

    Deploy pipeline (#2)
    * Fix native compilation
    
    * add deploy and code review pipelines
    
    * removed deploy to prod
    
    ---------
    
    Co-authored-by: Giovanni <g.mancini.0@gmail.com>

--- src/main/java/it/pagopa/cstar/iomock/IoMockApplication.java
+++ src/main/java/it/pagopa/cstar/iomock/IoMockApplication.java
@@ -2,7 +2,10 @@ package it.pagopa.cstar.iomock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
+
+@ImportRuntimeHints(ReflectionConfig.class)
 @SpringBootApplication
 public class IoMockApplication {
 
---
+++ src/main/java/it/pagopa/cstar/iomock/ReflectionConfig.java
@@ -0,0 +1,13 @@
+package it.pagopa.cstar.iomock;
+
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.boot.logging.logback.RootLogLevelConfigurator;
+
+public class ReflectionConfig implements RuntimeHintsRegistrar {
+    @Override
+    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+        hints.reflection().registerType(RootLogLevelConfigurator.class, MemberCategory.values());
+    }
+} 
