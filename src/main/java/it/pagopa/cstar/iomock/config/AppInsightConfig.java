947d8583513430349068531e3106fd447b6bea80
petretiandrea <petretiandrea@gmail.com>
2023-09-14 11:25:30 +0200

    disable exporter otlp

--- src/main/java/it/pagopa/cstar/iomock/config/AppInsightConfig.java
+++ src/main/java/it/pagopa/cstar/iomock/config/AppInsightConfig.java
@@ -2,17 +2,15 @@ package it.pagopa.cstar.iomock.config;
 
 import com.azure.monitor.opentelemetry.exporter.AzureMonitorExporterBuilder;
 import io.opentelemetry.exporter.logging.LoggingMetricExporter;
-import io.opentelemetry.exporter.logging.LoggingSpanExporter;
 import io.opentelemetry.sdk.logs.export.LogRecordExporter;
 import io.opentelemetry.sdk.metrics.export.MetricExporter;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AppInsightConfig implements BeanPostProcessor {
+public class AppInsightConfig {
 
   private final AzureMonitorExporterBuilder azureMonitorExporterBuilder;
 
@@ -26,10 +24,10 @@ public class AppInsightConfig implements BeanPostProcessor {
 //    return LoggingSpanExporter.create();
 //  }
 //
-//  @Bean
-//  public MetricExporter loggingMetricExporter() {
-//    return LoggingMetricExporter.create();
-//  }
+/*  @Bean
+  public MetricExporter loggingMetricExporter() {
+    return LoggingMetricExporter.create();
+  }*/
 
   @Bean
   public SpanExporter azureSpanProcessor() {
