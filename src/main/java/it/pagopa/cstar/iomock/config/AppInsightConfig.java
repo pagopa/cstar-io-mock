213a7da318fdfcc4b8711242c687c520e7f96d95
petretiandrea <petretiandrea@gmail.com>
2023-09-18 11:51:53 +0200

    allows testing without otel

--- src/main/java/it/pagopa/cstar/iomock/config/AppInsightConfig.java
+++ src/main/java/it/pagopa/cstar/iomock/config/AppInsightConfig.java
@@ -1,13 +1,18 @@
 package it.pagopa.cstar.iomock.config;
 
 import com.azure.monitor.opentelemetry.exporter.AzureMonitorExporterBuilder;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.spring.webflux.v5_3.SpringWebfluxTelemetry;
 import io.opentelemetry.sdk.logs.export.LogRecordExporter;
 import io.opentelemetry.sdk.metrics.export.MetricExporter;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.WebFilter;
 
+@ConditionalOnProperty(value = "applicationinsights.connectionstring")
 @Configuration
 public class AppInsightConfig {
 
@@ -31,5 +36,15 @@ public class AppInsightConfig {
   @Bean
   public LogRecordExporter azureLogRecordExporter() {
     return azureMonitorExporterBuilder.buildLogRecordExporter();
+  }
+
+  @Bean
+  public SpringWebfluxTelemetry springWebfluxTelemetry(OpenTelemetry openTelemetry) {
+    return SpringWebfluxTelemetry.builder(openTelemetry).build();
+  }
+
+  @Bean
+  public WebFilter webFilter(SpringWebfluxTelemetry webfluxTelemetry) {
+    return webfluxTelemetry.createWebFilterAndRegisterReactorHook();
   }
 }
